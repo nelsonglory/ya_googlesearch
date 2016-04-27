82c03eeb5661ce889567d81f66d1c91203a2b916
nelsonglory <roderick.braun@ph-freiburg.de>
2016-04-27 12:06:26 +0200

    removed deprecated loadTCA function

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
 
 
@@ -14,7 +13,6 @@ t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Google search");
 if (TYPO3_MODE=="BE")	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_yagooglesearch_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_yagooglesearch_pi1_wizicon.php';
 
 
-t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi2']='layout,select_key';
 
 
